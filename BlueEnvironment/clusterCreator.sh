3aa823a61041bf82a2fa466e0596e7f1d0d4eb50
Josiel Souza <josielbr@gmail.com>
2021-04-07 06:59:27 +0100

    Including blue, green environments and part of config file

---
+++ BlueEnvironment/clusterCreator.sh
@@ -0,0 +1,5 @@
+eksctl create cluster \
+--name=Kubernetes-devops-capstone \
+--nodes=2 \
+--region=eu-west-2  \
+--managed --node-type=t2.small
---
+++ BlueEnvironment/upload_docker.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+# This file tags and uploads an image to Docker Hub
+
+# Assumes that an image is built via `run_docker.sh`
+
+# Step 1:
+# Create dockerpath
+dockerpath="josielbr/devops-capstone-blue"
+
+# Step 2:  
+# Authenticate & tag
+echo "Docker ID and Image: $dockerpath"
+docker tag devops-capstone-blue $dockerpath
+docker login --username josielbr $DOCKER_PWD
+# Step 3:
+# Push image to a docker repository
+docker push $dockerpath
